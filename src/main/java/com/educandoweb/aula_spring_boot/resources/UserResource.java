14966be2f33e9c38a983639e1102c39ca4d2c88c
jonas.ramos <jrenovaveis@gmail.com>
2024-09-11 20:28:46 -0300

    criação da classe User e seus recursos iniciais

---
+++ src/main/java/com/educandoweb/aula_spring_boot/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.aula_spring_boot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.aula_spring_boot.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Jonas Ramos", "jonas@gmail.com", "888888888", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
