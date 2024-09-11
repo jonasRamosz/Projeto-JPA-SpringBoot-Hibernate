aecea2e99185e6c1cb01a48c69b66c98f9a4eb83
jonas.ramos <jrenovaveis@gmail.com>
2024-09-11 20:08:28 -0300

    chore: primeiras configs

---
+++ src/test/java/com/educandoweb/aula_spring_boot/AulaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aula_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
